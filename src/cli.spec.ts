01c321fde8619907328d5fcf1cede3283e0f219d
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-01-15 21:35:20 +0900

    test: add snapshot test for init command

--- src/cli.spec.ts
+++ src/cli.spec.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'child_process'
-import { describe, expect, it } from 'vitest'
+import fs from 'fs'
+import { describe, expect, it, beforeEach, afterEach } from 'vitest'
 
 function runCli(arg: string): string {
   return execSync(`tsx src/cli.ts ${arg}`).toString()
@@ -8,5 +9,20 @@ function runCli(arg: string): string {
 describe('cli help command snapshot test', () => {
   it.each(['--help', '-h', 'init --help', 'init -h'])('%s', (arg) => {
     expect(runCli(arg)).toMatchSnapshot()
+  })
+})
+
+describe('init command snapshot test', () => {
+  beforeEach(() => {
+    fs.mkdirSync('tmp/src', { recursive: true })
+  })
+
+  afterEach(() => {
+    fs.rmSync('tmp', { recursive: true })
+  })
+
+  it('normal', () => {
+    expect(runCli('init -p tmp/src/wire.ts')).toMatchSnapshot()
+    expect(fs.readFileSync('tmp/src/wire.ts', 'utf-8')).toMatchSnapshot()
   })
 })
